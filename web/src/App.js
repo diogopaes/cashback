0f7a19d19ce45fd199e1e6d630d95215e619bd39
Diogo Paes <diogopaes20@gmail.com>
2020-04-24 03:03:41 -0300

    Configured editorconfig, eslint and prettier

--- web/src/App.js
+++ web/src/App.js
@@ -3,11 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello World
-        </h1>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
