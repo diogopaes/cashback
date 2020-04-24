5ee781f2b3834f0a735e1dc53ffbba3ee5ce2bf9
Diogo Paes <diogopaes20@gmail.com>
2020-04-24 18:57:12 -0300

    Ajusted form and input

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '../../assets/logodetalhe.svg';
 
 import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <div>
         <img src={logo} alt="CashBack" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h2>Acessar</h2>
           <h4>
             Olá revendedor(a), aqui você podera cadastrar suas compras e
             visualizar seu cashback.
           </h4>
-          <input type="email" placeholder="Seu e-mail" />
-          <input type="password" placeholder="Sua Senha" />
+          <Input name="email" type="email" placeholder="Seu e-mail" />
+          <Input name="password" type="password" placeholder="Sua Senha" />
 
           <button type="submit">Acessar</button>
-        </form>
+        </Form>
 
         <span>
           Ainda não tem conta? <Link to="/signup">Criar agora</Link>
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '../../assets/logodetalhe.svg';
 
 import { Container } from './styles';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <div>
         <img src={logo} alt="CashBack" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h2>Cadastrar</h2>
           <h4>Olá revendedor(a), crie sua conta.</h4>
-          <input type="email" placeholder="Seu e-mail" />
-          <input type="cpf" placeholder="Seu cpf" />
-          <input type="password" placeholder="Sua Senha" />
-          <input type="password" placeholder="Repetir Senha" />
+          <Input name="email" type="email" placeholder="Seu e-mail" />
+          <Input name="cpf" type="cpf" placeholder="Seu cpf" />
+          <Input name="password" type="password" placeholder="Sua Senha" />
 
           <button type="submit">Cadastrar</button>
-        </form>
+        </Form>
 
         <span>
           Já tem uma conta? <Link to="/">Acessar</Link>
