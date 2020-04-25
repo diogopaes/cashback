1155c01dd24cc66d73da77044bb4fb1aff919633
Diogo Paes <diogopaes20@gmail.com>
2020-04-25 16:44:47 -0300

    Added custom erros messages

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '../../../services/history';
 import api from '../../../services/api';
@@ -22,6 +23,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação');
     yield put(signFailure());
   }
 }
