d4b0852077c6dc9fcd1ea8307c25f82be6ee8098
Diogo Paes <diogopaes20@gmail.com>
2020-04-25 16:31:35 -0300

    configured loading login

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -3,23 +3,27 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '../../../services/history';
 import api from '../../../services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { identifier, password } = payload;
+  try {
+    const { identifier, password } = payload;
 
-  const response = yield call(api.post, 'auth/local', {
-    identifier,
-    password,
-  });
+    const response = yield call(api.post, 'auth/local', {
+      identifier,
+      password,
+    });
 
-  const { jwt, user } = response.data;
+    const { jwt, user } = response.data;
 
-  api.defaults.headers.Authorization = `Bearer ${jwt}`;
+    api.defaults.headers.Authorization = `Bearer ${jwt}`;
 
-  yield put(signInSuccess(jwt, user));
+    yield put(signInSuccess(jwt, user));
 
-  history.push('/dashboard');
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
